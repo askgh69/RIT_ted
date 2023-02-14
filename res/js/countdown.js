345251bf5ba0b358ebffedec21630fcff913b914
Chinmay Nadgir <69594137+chinmaynadgir@users.noreply.github.com>
2023-02-14 19:09:48 +0530

    Update countdown.js

--- res/js/countdown.js
+++ res/js/countdown.js
@@ -1,5 +1,5 @@
 $('.countdown-timer').countdown({
-    date: "February 23, 2023 10:00:00",
+    date: "February 25, 2023 10:00:00",
 
     render: function(data) {
         $(".days .value").html(this.leadingZeros(data.days, 2));
